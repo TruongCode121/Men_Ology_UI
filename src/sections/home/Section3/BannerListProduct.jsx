815f6c953b9e0e55912c7f210a6ccbfb2cc59e12
truongCodeGa <truongntx99@gmail.com>
2024-08-02 15:22:24 +0700

    fix img mobie

--- src/sections/home/Section3/BannerListProduct.jsx
+++ src/sections/home/Section3/BannerListProduct.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import image_140 from "../../../../public/home/image_140.jpg";
 import image_140_mobie from "../../../../public/home/image_140_mobie.png";
+import banner_mobie_section3 from "../../../../public/home/banner_mobie_section3.jpg";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import handleUseScrollTrigger from "@/hooks/useScrollTriggerFromTo";
@@ -27,11 +28,11 @@ const BannerListProduct = () => {
           ></Image>
         </div>
       </div>
-      <div className="bg-white overflow-hidden w-[21.9375rem] sm:w-full h-[14.3125rem] rounded-2xl mt-[1.5rem] sm:hidden">
+      <div className="bg-white overflow-hidden w-[21.9375rem] sm:w-full h-[14.3125rem] rounded-2xl mt-[1.5rem] sm:hidden p-[0.5rem]">
         <Image
-          src={image_140_mobie}
+          src={banner_mobie_section3}
           alt="image_140"
-          className="w-full rounded-xl lg:hidden inline-block -mt-[2.4rem] scale-x-[1.05] scale-y-[1.05]"
+          className="w-full rounded-xl lg:hidden inline-block "
         ></Image>
       </div>
     </section>
